4e9ab331d27e918441f94334260f7525a1d1aeea
Igor Soloydenko <soloydenko@gmail.com>
2017-11-24 17:17:03 -0800

    Make package globally installable (as a command)

--- src/tspck.ts
+++ src/tspck.ts
@@ -6,5 +6,5 @@ const cmd =
   commander
     .description(`tspck is a tool for dealing with TypeScript packages.`)
     .command(`init`, `Initializes a new package.`)
-    .version(`1.0.0-alpha.0`)
+    .version(`1.0.0-alpha.3`)
     .parse(process.argv);
