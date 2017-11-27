4e66164a602619ca4cde5c4989e0e93fa4f9f566
Igor Soloydenko <soloydenko@gmail.com>
2017-11-26 21:33:28 -0800

    Use bin/ instead of dist/ otherwise npm won't recognize subcommands

--- src/tspck-init.ts
+++ src/tspck-init.ts
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
+// import { ChildProcess, spawn } from 'child_process';
 import * as commander from 'commander';
 
-const cmd =
+const initCommand =
   commander
     // .description(`tspck is a tool for dealing with TypeScript packages.`)
-    .option(`ooo`, `OOOOO.`)
+    .option(`-o, --ooo`, `OOOOO.`)
     .parse(process.argv);
+
+// const gitInit = spawn(`git`, [`init`]);
+
+// // tslint:disable:no-console
+// gitInit.stdout.on( 'data', data => {
+//   console.log( `stdout: ${data}` );
+// });
+
+// gitInit.stderr.on( 'data', data => {
+//   console.log( `stderr: ${data}` );
+// });
+
+// gitInit.on( 'close', code => {
+//   console.log( `child process exited with code ${code}` );
+// });
--- src/tspck.ts
+++ src/tspck.ts
@@ -2,9 +2,9 @@
 
 import * as commander from 'commander';
 
-const cmd =
+const tspckCommand =
   commander
     .description(`tspck is a tool for dealing with TypeScript packages.`)
     .command(`init`, `Initializes a new package.`)
-    .version(`1.0.0-alpha.3`)
+    .version(`1.0.0-alpha.14`)
     .parse(process.argv);
