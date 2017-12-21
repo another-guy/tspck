ca68eb11ce21517fe75ba0d0403e4400212a2006
Igor Soloydenko <soloydenko@gmail.com>
2017-12-20 20:21:21 -0800

    Output tspck init steps in color

--- src/tspck-init.ts
+++ src/tspck-init.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import * as commander from 'commander';
 
 import { copyFile } from './file-system/copy-file';
@@ -25,33 +26,48 @@ const initCommand =
 
 // TODO Extract file paths
 
-// TODO Extract setup states
-
-// TODO colorize the console output
+const log = (...text: string[]): void => console.log(chalk.cyan(...text));
 
 (async () => {
 
+  log(`Starting tspck init`);
+
   try {
 
+    log(`Initializing Git`);
     await gitInit();
+    log(`Creating a .gitignore file`);
     copyFile(__dirname, `assets`, `gitignore`, ``, `.gitignore`);
 
+    log(`Creating directory structure`);
     await createDirectories();
+
+    log(`Creating karma configuration`);
     copyFile(__dirname, `assets`, `karma.conf.js`, `.config`, `karma.conf.js`);
+    log(`Creating tslint configuration`);
     copyFile(__dirname, `assets`, `tslint.json`, `.config`, `tslint.json`);
+    log(`Creating vscode configuration`);
     copyFile(__dirname, `assets`, `settings.json`, `.vscode`, `settings.json`);
 
+    log(`Initializing NPM package`);
     await npmInit();
+    log(`Installing NPM packages`);
     await npmInstallPackages();
+    log(`Overriding NPM scripts`);
     await overrideNpmScripts();
 
+    log(`Initializing TypeScript compiler configuration`);
     await tscInit('./');
+    log(`Overriding default TypeScript configuration keys`);
     await overrideTsConfig();
 
+    log(`Creating initial Git commit for generated package`);
     await gitCommit();
 
   } catch (error) {
     console.warn(`Critical failure in on of the steps: ${ JSON.stringify(error, null, 2) }`);
   }
+
+  log(`tspck init is over now`);
 
 })();
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = `1.0.0-alpha.25`;
+export const VERSION = `1.0.0-beta.26`;
